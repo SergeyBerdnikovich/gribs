14efdd3fccb3d3919ba96c6c893d0514a99db2b7
SergeyBerdnikovich <atom@tut.by>
2013-06-21 13:59:23 +0300

    redirect to reports_mashrooms_path() after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!, :except => [:welcome]
+
+  def after_sign_in_path_for(resource)
+    reports_mashrooms_path()
+  end
 end
